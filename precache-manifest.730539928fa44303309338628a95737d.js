72ed238dd5f11216b3cf08950e69bf8ef2c36023
Jacob Ross <jross263@uwo.ca>
2019-10-05 16:13:20 -0400

    first commit

---
+++ precache-manifest.730539928fa44303309338628a95737d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3334d5fc86cd692e4909dea160ddfcf3",
+    "url": "/TicTacToeReact/index.html"
+  },
+  {
+    "revision": "b36e5567b996aaeb248c",
+    "url": "/TicTacToeReact/static/css/main.55d72bbf.chunk.css"
+  },
+  {
+    "revision": "a37ecef15e23da5619da",
+    "url": "/TicTacToeReact/static/js/2.5296576a.chunk.js"
+  },
+  {
+    "revision": "b36e5567b996aaeb248c",
+    "url": "/TicTacToeReact/static/js/main.141ae670.chunk.js"
+  },
+  {
+    "revision": "bd2e0b82f7e0095cdaa9",
+    "url": "/TicTacToeReact/static/js/runtime-main.52f543dd.js"
+  }
+]);
